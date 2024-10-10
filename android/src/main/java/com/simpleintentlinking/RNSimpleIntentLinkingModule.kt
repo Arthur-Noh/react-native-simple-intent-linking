d200e8e061807fb50d9ac69cb872e0c5e1b86ae2
Arthur-Noh <dkagod97@gmail.com>
2024-10-10 11:03:04 +0900

    feat: update to kotlin

---
+++ android/src/main/java/com/simpleintentlinking/RNSimpleIntentLinkingModule.kt
@@ -0,0 +1,46 @@
+package com.simpleintentlinking;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.List;
+
+class RNSimpleIntentLinkingModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    private val reactContext: ReactApplicationContext = reactContext
+
+    override fun getName(): String {
+        return "RNSimpleIntentLinking"
+    }
+
+    @ReactMethod
+    fun openURL(url: String, promise: Promise) {
+        try {
+            val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            reactContext.currentActivity?.startActivity(intent)
+            promise.resolve(null)
+        } catch (exception: Exception) {
+            promise.reject(exception)
+        }
+    }
+
+    @ReactMethod
+    fun canOpenURL(url: String, promise: Promise) {
+        try {
+            val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+            val manager: PackageManager = reactContext.packageManager
+            val infoList: List<ResolveInfo> = manager.queryIntentActivities(intent, 0)
+
+            promise.resolve(infoList.isNotEmpty())
+        } catch (exception: Exception) {
+            promise.reject(exception)
+        }
+    }
+}
---
+++ android/src/main/java/com/simpleintentlinking/RNSimpleIntentLinkingPackage.kt
@@ -0,0 +1,16 @@
+package com.simpleintentlinking
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class RNSimpleIntentLinkingPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(RNSimpleIntentLinkingModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
